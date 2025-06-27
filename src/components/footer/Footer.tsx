8f8323e51ba90939cb3ec1ab76703f9b3fd5d30d
ElectroCERP <carloscer27r@gmail.com>
2025-06-27 12:34:22 -0300

    Create a footer with my socials

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,43 @@
+import { GithubIcon, LinkedinIcon, MailIcon } from "../icons/icons";
+
+interface FooterProps {
+  email: string;
+  github: string;
+  linkedin: string;
+}
+
+export const Footer = ({ email, github, linkedin }: FooterProps) => {
+  return (
+    <footer className="absolute bottom w-full bg-black flex flex-wrap justify-center gap-4 items-center text-white py-4 px-4 text-center">
+      <div className="flex items-center gap-4">
+        <a
+          href={github}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-gray-300"
+        >
+          <GithubIcon />
+        </a>
+        <a
+          href={linkedin}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-gray-300"
+        >
+          <LinkedinIcon />
+        </a>
+        <a
+          href={`mailto:${email}?subject=Contact%20from%20portfolio`}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-gray-300"
+        >
+          <MailIcon />
+        </a>
+      </div>
+      <p className="text-xs">
+        © {new Date().getFullYear()} Carlos Rojas. All rights reserved.
+      </p>
+    </footer>
+  );
+};
