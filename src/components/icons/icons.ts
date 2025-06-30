7018d01dcf46328abd63625f494e236a2c3a8998
ElectroCERP <carloscer27r@gmail.com>
2025-06-30 18:21:31 -0300

    Update icons barrel file

--- src/components/icons/icons.ts
+++ src/components/icons/icons.ts
@@ -2,3 +2,5 @@ export { AnimatedMenuIcon } from "./AnimatedMenuIcon";
 export { GithubIcon } from "./GithubIcon";
 export { LinkedinIcon } from "./LinkedinIcon";
 export { MailIcon } from "./MailIcon";
+export { LogoIcon } from "./LogoIcon";
+export { ArrowDownIcon } from "./ArrowDownIcon";
