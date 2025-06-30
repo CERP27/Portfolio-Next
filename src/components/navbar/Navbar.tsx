6682d18cc3bb5199becad9f1507533327173b1fe
ElectroCERP <carloscer27r@gmail.com>
2025-06-30 18:17:53 -0300

    Update Navbar styles and mobile functionality

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,19 +1,21 @@
 "use client";
 import Link from "next/link";
-import { useState } from "react";
-import { AnimatedMenuIcon } from "../icons/icons";
-import { AnimatePresence, motion } from "framer-motion";
+import { useState, useRef, useEffect } from "react";
+import { AnimatedMenuIcon, LogoIcon } from "../icons/icons";
+import { AnimatePresence, motion, useInView } from "framer-motion";
 
 export const Navbar = () => {
   const links = [
-    { label: "Home", href: "#home" },
+    { label: "Home", href: "#welcome" },
     { label: "About", href: "#about" },
+    { label: "Skills", href: "#skills" },
     { label: "Projects", href: "#projects" },
     { label: "Contact", href: "#contact" },
   ];
 
+  const ref = useRef(null);
+  const isNavInView = useInView(ref);
   const [isOpen, setIsOpen] = useState(false);
-
   const toggleMenu = () => setIsOpen(!isOpen);
 
   const renderLinks = () =>
@@ -27,18 +29,26 @@ export const Navbar = () => {
         {link.label}
       </Link>
     ));
+
+  useEffect(() => {
+    setIsOpen(false);
+  }, [isNavInView]);
+
   return (
     <nav
-      className="bg-white shadow-md sticky top-0 z-50 w-full"
+      ref={ref}
+      className={`bg-[#0e0b1d]/75 ${
+        !isOpen && "shadow-md"
+      } sticky top-0 z-50 w-full`}
       role="navigation"
     >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between h-16 items-center">
-          <h1 className="text-2xl font-bold text-black">
-            {"<"}CRDev{">"}
-          </h1>
+        <div className="flex h-16 justify-between items-center">
+          <LogoIcon fill="#d1c4e9" className="h-24 w-24" />
 
-          <div className="hidden md:flex gap-6">{renderLinks()}</div>
+          <div className="hidden md:flex text-[#d1c4e9] gap-6">
+            {renderLinks()}
+          </div>
 
           <button
             onClick={toggleMenu}
@@ -54,13 +64,13 @@ export const Navbar = () => {
           {isOpen && (
             <motion.div
               key="mobile-menu"
-              initial={{ opacity: 0, y: -5 }}
+              initial={{ opacity: 0 }}
               animate={{ opacity: 1, y: 0 }}
-              exit={{ opacity: 0, y: -5 }}
+              exit={{ opacity: 0 }}
               transition={{ duration: 0.2 }}
-              className=" md:hidden animate-[fade-in-down_0.3s_ease-in-out] absolute w-full right-0 z-50"
+              className=" md:hidden animate-[fade-in-down_0.3s_ease-in-out] text-[#d1c4e9] absolute w-full right-0 z-50"
             >
-              <div className="bg-white flex flex-col gap-4 p-4 items-center  shadow-md rounded-b-2xl">
+              <div className="bg-[#0e0b1d]/75 flex flex-col gap-4 p-4 items-center  shadow-md rounded-b-2xl">
                 {renderLinks()}
               </div>
             </motion.div>
