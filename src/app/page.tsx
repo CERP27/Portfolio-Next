2a86e7103500acda8c2cff7656617e47e070f12c
ElectroCERP <carloscer27r@gmail.com>
2025-07-03 13:51:58 -0300

    Make Homepage a static page that renders all the dev data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,79 @@
-import Image from "next/image";
+export const dynamic = "force-static";
 
-export default function Home() {
+import { AboutSection } from "@/components/about/AboutSection";
+import { ContactMeSection } from "@/components/contact/ContactMeSection";
+import { Footer } from "@/components/footer/Footer";
+import { HeroSection } from "@/components/hero/HeroSection";
+import { Navbar } from "@/components/navbar/Navbar";
+import { ProjectSection } from "@/components/projects/ProjectSection";
+import { SkillSection } from "@/components/skills/SkillsSection";
+import { IProject } from "@/interfaces/projects.interface";
+import { ISkill, SkillType } from "@/interfaces/skills.interface";
+import { prismaClient } from "@/lib/prisma";
+
+const Home = async () => {
+  const personalData = await prismaClient.personalData.findFirst({
+    include: {
+      skills: true,
+      projects: true,
+    },
+  });
+
+  const groupedSkills = personalData!.skills.reduce<Record<string, ISkill[]>>(
+    (skills, { name, logo, type }) => {
+      if (!skills[type]) {
+        skills[type] = [];
+      }
+      skills[type].push({
+        logo,
+        name,
+        type: type as SkillType,
+      });
+      return skills;
+    },
+    {}
+  );
+  
+  const projects: IProject[] = personalData!.projects.map(
+    ({ title, description, url, repoUrl, imageUrl, technologies, id }) => {
+      return {
+        id,
+        description,
+        imageUrl,
+        technologies,
+        title,
+        url,
+        repoUrl,
+      };
+    }
+  );
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="min-h-screen items-center justify-items-center ">
+      <Navbar />
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+      <main id="welcome" className="flex flex-col pb-4">
+        <HeroSection name={personalData!.name} />
+
+        <AboutSection description={personalData!.about} />
+
+        <SkillSection skills={groupedSkills} />
+
+        <ProjectSection projects={projects} />
+
+        <ContactMeSection
+          email={personalData!.email}
+          githubUrl={personalData!.github}
+          linkedInUrl={personalData!.linkedin}
+        />
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <Footer
+        email={personalData!.email}
+        github={personalData!.github}
+        linkedin={personalData!.linkedin}
+      />
     </div>
   );
-}
+};
+
+export default Home;
