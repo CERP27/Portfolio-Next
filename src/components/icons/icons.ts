73868e03468b03271b95a844b1b8772af21e9e00
ElectroCERP <carloscer27r@gmail.com>
2025-07-03 12:59:55 -0300

    Create Loading and WWW Icons

--- src/components/icons/icons.ts
+++ src/components/icons/icons.ts
@@ -4,3 +4,5 @@ export { LinkedinIcon } from "./LinkedinIcon";
 export { MailIcon } from "./MailIcon";
 export { LogoIcon } from "./LogoIcon";
 export { ArrowDownIcon } from "./ArrowDownIcon";
+export { LoadingIcon } from "./LoadingIcon";
+export { WorldWideWebIcon } from "./WorldWideWebIcon";
